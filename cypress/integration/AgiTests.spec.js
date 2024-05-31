9838ad2ad7e121c7acb6cb1efb82d2edc3255ad4
RENATO <renato.ars88@hotmail.com>
2024-05-31 17:44:12 -0300

    Adicionando integração continua no Githubsctions

--- cypress/integration/AgiTests.spec.js
+++ cypress/integration/AgiTests.spec.js
@@ -9,6 +9,16 @@ describe('Teste para sinqia', function(){
         cy.get('.entry-title > a').should('be.visible').click()
        
     })
+    it('ao clicar no resultado ele deve abrir a página', function(){
+        cy.get('.slide-search > .ast-icon > svg').click()
+        cy.get('#search-field').type('Antena Digital{enter}')
+        cy.get('.entry-title > a').should('be.visible')  
+        cy.get('entry-title > a').click() 
+        cy.wait(2000)
+    })
+    it.only('título do artigo deve estar correto',function(){
+        cy.visit('https://blogdoagi.com.br/antena-digital-do-governo/')
+        cy.get('.entry-title').should('be.visible')
+    })
 
-    
 })
